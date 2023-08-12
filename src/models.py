ac610bdb8d484135dd59ed068106e90c84dc62f8
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-08-12 22:12:24 +0000

    Modificacion de la relaciones de Tablas

--- src/models.py
+++ src/models.py
@@ -7,28 +7,27 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'user'
+class Users(Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
-    password = Column(String(20), nullable=False)
-    #Relacion con la Tabla Favorites
-    id_favorites = Column(ForeignKey("favorites.id"))
-    favorites = relationship( "Favorites", back_populates = "user")
+    password = Column(String(50), nullable=False)
+    subscription_date = Column(String(50), nullable=False)
+    #Relacion con la tabla de Favorites (Child)
+    favorites = relationship("Favorites", back_populates="users")
 
 class Characters(Base):
     __tablename__ = 'characters'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    full_name = Column(String(50), nullable=False)
+    name = Column(String(50), nullable=False)
     description = Column(String(250), nullable=False)
-    homeworld = Column(String(10), nullable=False)
-    #Relacion con la Tabla Favorites
+    #Relacion con la tabla de Favorites (Child)
     favorites = relationship("Favorites", back_populates="characters")
 
 class Planets(Base):
@@ -38,21 +37,20 @@ class Planets(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     population = Column(Integer, nullable=False)
-    #Relacion con la Tabla Favorites
+    #Relacion con la tabla de Favorites (Child)
     favorites = relationship("Favorites", back_populates="planets")
     
-class Favorites(Base):
+class Favorites(Base): #(Father)
     __tablename__ = 'favorites'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    #Relacion con la Tabla User
     id_user = Column(ForeignKey("users.id")) 
     users = relationship( "Users", back_populates = "favorites")
-    #Relacion con la Tabla Character
+    #Relacion con la tabla de Characters (Child)
     id_character = Column(ForeignKey("characters.id"))
     character = relationship("Characters", back_populates = "favorites")
-    #Relacion con la Tabla Planets
+    #Relacion con la tabla de Planets (Child)
     id_planets = Column(ForeignKey("planets.id"))
     planets = relationship( "Planets", back_populates = "favorites")
 
